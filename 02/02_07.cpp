5de2dc447521aca0d7070ab69d6e9f784272ee31
LordMustang <elionass58@gmail.com>
2022-01-14 11:46:45 +0100

    Added 2.7

---
+++ 02/02_07.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::cout << "Who goes with F\145rgus?\012" << std::endl;
+	std::cout << "Who goes with Fergus?\n" << std::endl;
+
+	long double b = 3.14e1L;
+	// 1024f is neither a float nor an int
+	long double d = 3.14L;
+	return 0;
+}
