db1bea548bdc40805f254e9cdf4983fc324b65b4
LordMustang <elionass58@gmail.com>
2022-01-14 12:22:21 +0100

    Added 2.10

---
+++ 02/02_10.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+
+std::string global_str; // empty string
+int global_int; // 0
+
+int main() {
+	int local_int; // undefined
+	std::string local_str; // undefined
+}
