2c86071920e8951d303e87af26a3c4a98764f4e8
LordMustang <elionass58@gmail.com>
2022-01-14 14:00:09 +0100

    Added 2.12

---
+++ 02/02_12.cpp
@@ -0,0 +1,8 @@
+int main() {
+	int double = 3.14; // invalid
+	int _; // valid inside a function
+	int catch-22; // invalid
+	int 1_or_2 = 1; // invalid
+	double Double = 3.14; // valid
+	return 0;
+}
