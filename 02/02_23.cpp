d0a7bcbf4f1104fb4bd4e23b27586f4667570ab9
LordMustang <elionass58@gmail.com>
2022-01-17 14:07:52 +0100

    Added 2.23

---
+++ 02/02_23.cpp
@@ -0,0 +1,6 @@
+/* No, you can't. Why?
+ * Because it would be expensive to maintain meta data about what constitutes a valid pointer and what doesn't,
+ * and in C++ you don't pay for what you don't want
+ * 
+ * https://stackoverflow.com/a/17202622/10787114
+*/
