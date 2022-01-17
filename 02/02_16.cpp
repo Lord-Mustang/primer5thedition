95c0de5bcb32e7673f3471d717861501eeafd1a5
LordMustang <elionass58@gmail.com>
2022-01-17 11:35:49 +0100

    Added 2.16

---
+++ 02/02_16.cpp
@@ -0,0 +1,5 @@
+/* r2 = 3.14159; // Valid. Assigns the literal to the referenced object
+ * r2 = r1; // Valid. Assigns the literal to the referenced object
+ * i = r2; // Valid. Assigns the literal to the referenced object
+ * r1 = d; // Valid. Assigns the literal to the referenced object
+*/
